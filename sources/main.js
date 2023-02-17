ff0461b5fff71c46e7afe1e04a89df0c1ea46c1a
inessben <ine.benss@gmail.com>
2023-02-17 15:09:29 +0100

    on finishing

--- sources/main.js
+++ sources/main.js
@@ -249,7 +249,7 @@ gsap.to(logo.rotation, {
 const sun = new THREE.Vector3()
 
 // add the sea
-const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 )
 const water = new Water(waterGeometry, 
     {
     textureWidth: 512,
@@ -272,11 +272,11 @@ scene.add( water )
 const sky = new Sky()
 sky.scale.setScalar( 10000 )
 scene.add( sky )
-const skyUniforms = sky.material.uniforms;
-skyUniforms[ 'turbidity' ].value = 10;
-skyUniforms[ 'rayleigh' ].value = 2;
-skyUniforms[ 'mieCoefficient' ].value = 0.005;
-skyUniforms[ 'mieDirectionalG' ].value = 0.8;
+const skyUniforms = sky.material.uniforms
+skyUniforms[ 'turbidity' ].value = 10
+skyUniforms[ 'rayleigh' ].value = 2
+skyUniforms[ 'mieCoefficient' ].value = 0.005
+skyUniforms[ 'mieDirectionalG' ].value = 0.8
 const parameters = {
     elevation: 2,
     azimuth: 180
